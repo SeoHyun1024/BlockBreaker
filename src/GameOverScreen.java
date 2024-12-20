eb8ad3cfb1c5ec4071c16973ff96b703b4bfc2f4
Lee seohuyn <leesophy0805@gmail.com>
2024-12-21 02:48:09 +0900

    GameOverScreen - 별 찍기 & 트릐 그리기

--- src/GameOverScreen.java
+++ src/GameOverScreen.java
@@ -61,6 +61,9 @@ public class GameOverScreen extends JPanel{
               g2.drawString(instructions, (getWidth() - instructionsWidth) / 2 + 2, 438 + 2);
               g2.setColor(new Color(243, 223, 45));
               g2.drawString(instructions, (getWidth() - instructionsWidth) / 2, 438);
+
+              drawTreeRow(g2, getWidth(), getHeight() - 230);
+              drawStar(g2);
           }
         };
         p.setFocusable(true);
@@ -72,5 +75,59 @@ public class GameOverScreen extends JPanel{
                 }
             }
         });
+    }
+
+    private  void drawTree(Graphics2D g2, int x, int y, int treeHeightOffset){
+        int triangleHeight = 48;
+        int triangleWidth = 70;
+
+        // 삼각형 그리기
+        for (int i = 0; i < 4; i++){
+            int[] xPoints = {x, x - triangleWidth /2 - i*i*5, x + triangleWidth / 2 + i*i*5};
+            int[] yPoints = {y + (i * triangleHeight ) - 40 - treeHeightOffset, y + (i * triangleHeight + triangleHeight) - treeHeightOffset, y + (i * triangleHeight +triangleHeight - treeHeightOffset )};
+            Polygon triangle = new Polygon(xPoints, yPoints, xPoints.length);
+            g2.setColor(Color.WHITE);
+            g2.fillPolygon(triangle);
+        }
+
+        // 나무 기둥 그리기
+        int trunkWidth = 20;
+        int trunkHeight = 60;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(x - trunkWidth / 2, y + triangleHeight*4 - 60, trunkWidth, trunkHeight + 40);
+    }
+
+    private void drawTreeRow(Graphics2D g2, int panelWidth, int y){
+        int numTrees = 10;
+        int treeWidth = 60;
+        int[] spacing = {25, 25, 20, 30, 20, 30, 20, 25, 20, 25 };
+        int[] heightOffset = {5, 40, 10, 30, 0, 35, 0, 55, 10, 20};
+
+
+        for (int i = 0; i < numTrees; i++) {
+            int totalWidth = numTrees * treeWidth + (numTrees - 1) * spacing[i];
+            int startX = (panelWidth - totalWidth) / 2;
+            int treeX = startX + i * (treeWidth + spacing[i]);
+            drawTree(g2, treeX, y, heightOffset[i]);
+        }
+    }
+
+    private  void drawStar(Graphics2D g2){
+        // 별 찍기
+        g2.setColor(new Color(240, 235, 192));
+        g2.fillOval(108, 73, 5, 5);
+        g2.fillOval(215, 108, 5, 5);
+        g2.fillOval(379, 68, 5, 5);
+        g2.fillOval(486, 103, 5, 5);
+        g2.fillOval(569, 33, 5, 5);
+        g2.fillOval(676, 68, 5, 5);
+        g2.fillOval(34, 219, 5, 5);
+        g2.fillOval(188, 255, 5, 5);
+        g2.fillOval(295, 290, 5, 5);
+        g2.fillOval(463, 252, 5, 5);
+        g2.fillOval(305, 214, 5, 5);
+        g2.fillOval(627, 212, 5, 5);
+        g2.fillOval(595, 319, 5, 5);
+        g2.fillOval(734, 247, 5, 5);
     }
 }
